af137557761dc8ce075a4317369da373cf5add3b
aldeir <aldeir.afs@dpf.gov.br>
2022-02-12 21:44:23 -0300

    ajuste

--- src/main/java/com/algaFood/algaFoodapi/AlgaFoodApiApplication.java
+++ src/main/java/com/algaFood/algaFoodapi/AlgaFoodApiApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@ComponentScan({"main.controllers", "main.repositories"})
-//@EnableJpaRepositories("main.repositories")
+
 @SpringBootApplication
 public class AlgaFoodApiApplication {
 
