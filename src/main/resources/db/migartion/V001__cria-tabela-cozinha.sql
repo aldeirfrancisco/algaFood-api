d7454756441c664dac6fabc86de6952636eef2a5
aldeir <aldeir.afs@dpf.gov.br>
2022-03-12 18:51:27 -0300

    criando migrações

---
+++ src/main/resources/db/migartion/V001__cria-tabela-cozinha.sql
@@ -0,0 +1,6 @@
+create table cozinha (
+
+    id BIGINT  PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL
+
+)ENGINE=InnoDB default CHARSET =utf8;
---
+++ src/main/resources/db/migartion/V002__cria-tabela-cidade.sql
@@ -0,0 +1,7 @@
+create table cidade (
+
+     id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+     nome_cidade VARCHAR(80) NOT NULL,
+     nome_estado VARCHAR(80) NOT NULL
+
+)engine=InnoDB default charset=utf8;
