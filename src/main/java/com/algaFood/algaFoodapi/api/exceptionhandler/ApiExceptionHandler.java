338a7a70c0a41031deedbc9776b33759dca816af
aldeir <aldeir.afs@dpf.gov.br>
2022-03-14 21:23:44 -0300

    cirando  o metodo trataEntidadeEmUsoException

--- src/main/java/com/algaFood/algaFoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaFood/algaFoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algaFood.algaFoodapi.domain.exception.EntidadeEmUsoException;
 import com.algaFood.algaFoodapi.domain.exception.EntidadeNaoEncontradaExecption;
 import com.algaFood.algaFoodapi.domain.exception.NegocioException;
 
@@ -46,6 +47,18 @@ public class ApiExceptionHandler {
     			.msg("O tipo de mídia não é aceita.").build();
     	
     	return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    			.body(problema);
+    }
+    
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> trataEntidadeEmUsoException(
+    		EntidadeEmUsoException e){
+    	
+    	Problema problema = Problema.builder()
+    			.dataHora(LocalDateTime.now())
+    			.msg(e.getMessage()).build();
+    	
+    	return ResponseEntity.status(HttpStatus.CONFLICT)
     			.body(problema);
     }
 }
--- src/main/java/com/algaFood/algaFoodapi/domain/exception/EntidadeNaoEncontradaExecption.java
+++ src/main/java/com/algaFood/algaFoodapi/domain/exception/EntidadeNaoEncontradaExecption.java
@@ -1,9 +1,5 @@
 package com.algaFood.algaFoodapi.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value =  HttpStatus.NOT_FOUND)  //reason = "entidade nao encontrada"
 public abstract class EntidadeNaoEncontradaExecption extends NegocioException {
 	private static final long serialVersionUID = 1L;
 	
