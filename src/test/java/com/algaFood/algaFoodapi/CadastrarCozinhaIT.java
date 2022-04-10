de154b03dcb3e18787c54b70ce897e723fc79ae3
aldeir <aldeir.afs@dpf.gov.br>
2022-04-10 19:33:36 -0300

    teste de integração

--- src/test/java/com/algaFood/algaFoodapi/CadastrarCozinhaIT.java
+++ src/test/java/com/algaFood/algaFoodapi/CadastrarCozinhaIT.java
@@ -1,9 +1,10 @@
 package com.algaFood.algaFoodapi;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +14,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.matcher.ResponseAwareMatcher;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,12 +22,17 @@ public class CadastrarCozinhaIT {
   @LocalServerPort
   private int port;
   
+  @Before
+  public void setUp() {
+	  RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	  RestAssured.port =  port;
+	  RestAssured.baseURI = "/cozinhas";
+  }
+  
   @Test
   public void deveRetornarStatus200_QuandoConsultaCozinhas() {
-	  RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	 
 	  given()
-	      .basePath("/cozinhas")
-	      .port(port)
 	      .accept(ContentType.JSON)
 	   .when()
 	      .get()
@@ -37,16 +42,53 @@ public class CadastrarCozinhaIT {
   
   @Test
   public void deveConter4Cozinha_QuandoConsultaCozinhas() {
-	  RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-	  //Matchers: é uma biblioteca para escrever espresões com regras de corespondencia entre objeto.
+	 
+	  //Matchers: é uma biblioteca para escrever espresões com regras de correspondência entre objeto.
 	  given()
-	      .basePath("/cozinhas")
-	      .port(port)
 	      .accept(ContentType.JSON)
 	   .when()
 	      .get()
 	   .then()
 	      .body("", Matchers.hasSize(4))
 	      .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+  }
+  
+  @Test
+  public void deveRetornarStatus201_QuandoConsultaCozinhas() {
+	 
+	  given()
+	      .body("{\"nome\": \"Chinesa\"}")
+	      .contentType(ContentType.JSON)
+	      .accept(ContentType.JSON)
+	   .when()
+	      .post()
+	   .then()
+	      .statusCode(HttpStatus.CREATED.value());
+  }
+  
+  @Test
+  public void deveRetornarRespostaEStatusCorretos_QuandoConsultaCozinhasExistente() {
+	 
+	  given()
+	      .pathParam("cozinhaId", 1)
+	      .accept(ContentType.JSON)
+	   .when()
+	      .get("/{cozinhaId}")
+	   .then()
+	      .statusCode(HttpStatus.OK.value())
+	      .body("nome", equalTo("Indiana"));
+  }
+  
+  @Test
+  public void deveRetornarStatus404_QuandoConsultaCozinhasInexistente() {
+	 
+	  given()
+	      .pathParam("cozinhaId", 100)
+	      .accept(ContentType.JSON)
+	   .when()
+	      .get("/{cozinhaId}")
+	   .then()
+	      .statusCode(HttpStatus.NOT_FOUND.value());
+	     
   }
 }
