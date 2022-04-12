88fb146cc9d70b1bd0200f39c9e5719236b6f874
aldeir <aldeir.afs@dpf.gov.br>
2022-04-12 08:06:18 -0300

    criando maper de cozinha

---
+++ src/main/java/com/algaFood/algaFoodapi/api/mapper/CozinhaMapper.java
@@ -0,0 +1,44 @@
+package com.algaFood.algaFoodapi.api.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.algaFood.algaFoodapi.api.model.dto.CozinhaDTO;
+import com.algaFood.algaFoodapi.api.model.input.cozinha.CozinhaInput;
+import com.algaFood.algaFoodapi.domain.model.Cozinha;
+
+@Component
+public class CozinhaMapper {
+
+    final ModelMapper modelMapper;
+
+    public CozinhaMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public CozinhaDTO toDto(Cozinha cozinha){
+
+        return modelMapper.map(cozinha, CozinhaDTO.class);
+
+    }
+
+    public List<CozinhaDTO> toCollectionDto(List<Cozinha> cozinhas){
+
+        return cozinhas.stream().map(this::toDto).collect(Collectors.toList());
+    }
+
+    public Cozinha toEntity(CozinhaInput cozinhaInput){
+
+        return modelMapper.map(cozinhaInput, Cozinha.class);
+
+    }
+
+    public void copyToEntity(CozinhaInput cozinhaInput, Cozinha cozinha){
+
+        modelMapper.map(cozinhaInput, cozinha);
+
+    }
+}
